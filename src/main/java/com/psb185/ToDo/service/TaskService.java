f6fe0dbeadab584d69f42f7ae61a9479d45b4b29
pranav <pranavsinghbora@gmail.com>
2025-10-19 12:47:33 +0530

    Created methods for tasks in task service

---
+++ src/main/java/com/psb185/ToDo/service/TaskService.java
@@ -0,0 +1,56 @@
+package com.psb185.ToDo.service;
+
+import com.psb185.ToDo.entity.Task;
+import com.psb185.ToDo.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public List<Task> getTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(String title){
+        Task task = new Task();
+        task.setTitle(title);
+
+        return taskRepository.save(task);
+    }
+
+    public Task updateTitle(Long id, String title){
+
+        Task task = taskRepository.findById(id).orElse(null);
+
+        if (task != null){
+            task.setTitle(title);
+            return taskRepository.save(task);
+        }
+
+        return null;
+    }
+
+    public Task completeTask(Long id){
+        Task task = taskRepository.findById(id).orElse(null);
+
+        if (task != null){
+            task.setComplete(true);
+            return taskRepository.save(task);
+        }
+
+        return null;
+    }
+
+    public boolean deleteTask(Long id){
+        if (!taskRepository.existsById(id)) return false;
+
+        taskRepository.deleteById(id);
+        return true;
+    }
+}
