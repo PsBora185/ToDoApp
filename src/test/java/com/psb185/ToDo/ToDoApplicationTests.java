b0cdab2c53545577e8643af31adb04e6695ba886
pranav <pranavsinghbora@gmail.com>
2025-10-19 12:44:39 +0530

    Created todo Application

---
+++ src/test/java/com/psb185/ToDo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psb185.ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
